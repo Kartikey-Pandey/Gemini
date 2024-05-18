c4fb38031acdee8b6c4552af08cf82d4fd76bba9
Kartikey-Pandey <kartikeypdy1401@gmai.com>
2024-05-18 22:57:22 +0530

    first commit

---
+++ gimini-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebaar from './Components/Sidebar/Sidebar'
+import Main from './Components/Main/Main'
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <Sidebaar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
